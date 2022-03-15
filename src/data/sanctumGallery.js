bbb6b56cb8d8ccb463a3cb57eaccedfafc37e6ad
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2022-03-15 16:56:37 -0400

    add alt text & captions for Artist's Sanctum; minor style improvements for mobile and no-shadow display

--- src/data/sanctumGallery.js
+++ src/data/sanctumGallery.js
@@ -17,23 +17,31 @@ const sanctumGalleryData = {
     images: [
         {
             fileName: 'sanctum_9.jpg',
-            caption: '',
+            caption: 'To facilitate the design process and improve communication with the client, I built a simple 3D model of the escape room using Blender.',
+            altText: 'A top-down, wireframe view of a 3D model of the escape room',
         },
         {
             fileName: 'sanctum_3.jpg',
-            caption: '',
+            caption: `As seen in this 3D render, the escape room evoked the feeling of a Greco-Roman temple, where the Muses
+                would challenge and inspire artists. A one-way mirror could swing aside to reveal a hidden chamber.`,
+            altText: 'A 3D render of the escape room, featuring a tree, an easel, an altar, and some mirrors',
         },
         {
             fileName: 'sanctum_8.jpg',
-            caption: '',
+            caption: 'The final escape room, as it was constructed by the team at Xscape The Room. Some of my original design features were kept the same, while others were simplified to suit a tightened budget.',
+            altText: 'A photograph of the physical escape room, featuring a papier-mâché tree, a piano, and a framed painting with colored stripes',
         },
         {
             fileName: 'sanctum_composite_2.jpg',
-            caption: '',
+            caption: `The design process involved months of back-and-forth with the client, as we refined the puzzles and narrative.
+                Our goal was to provide a unique, immersive experience, with varied challenges that a group of players could
+                solve in parallel, and a few dramatic moments to bring everyone back together.`,
+            altText: 'A scattering of handwritten design notes, including sketches and blueprints',
         },
         {
             fileName: 'sanctum_7.jpg',
-            caption: '',
+            caption: `I provided the official description for "The Artist's Sanctum" on this promotional post card, and also played a significant role in choosing the name of the room.`,
+            altText: `A promotional postcard with short blurbs about three escape rooms - "The Basement," "The Safe House," and "The Artist's Sanctum"`,
         },
     ],
     links: [
